cffb2bf3b322446b426bf4e04028dc691e1cc8cc
JerryTZF <tzfforyou@163.com>
2022-07-11 09:33:50 +0800

    opt:优化监听器事件选择

--- app/Listener/AsyncRedisQueueListener.php
+++ app/Listener/AsyncRedisQueueListener.php
@@ -13,6 +13,8 @@ declare(strict_types=1);
 namespace App\Listener;
 
 use App\Lib\_Log\Log;
+use Hyperf\AsyncQueue\AnnotationJob;
+use Hyperf\AsyncQueue\Event\AfterHandle;
 use Hyperf\AsyncQueue\Event\FailedHandle;
 use Hyperf\AsyncQueue\Event\QueueLength;
 use Hyperf\AsyncQueue\Event\RetryHandle;
@@ -26,6 +28,8 @@ class AsyncRedisQueueListener implements ListenerInterface
     public function listen(): array
     {
         return [
+            // 消费队列事件
+            AfterHandle::class,
             // 队列长度信息事件
             QueueLength::class,
             // 消费失败事件
@@ -39,10 +43,19 @@ class AsyncRedisQueueListener implements ListenerInterface
         // 作用是：自动将 timeout 队列中消息移动到 waiting 队列中，等待下次消费
     }
 
-    public function process(object $event)
+    public function process(object $event): void
     {
-        switch (get_class($event)) {
-            case 'Hyperf\\AsyncQueue\\Event\\QueueLength':
+        switch (true) {
+            case $event instanceof AfterHandle:
+                $job = $event->message->job();
+                $jobClass = get_class($job);
+                if ($job instanceof AnnotationJob) {
+                    $jobClass = sprintf('Job[%s@%s]', $job->class, $job->method);
+                }
+                $message = sprintf('[%s] Processed %s.', date('Y-m-d H:i:s'), $jobClass);
+                Log::info($message);
+                break;
+            case $event instanceof QueueLength:
                 $message = sprintf('队列:%s;长度:%s', $event->key, $event->length);
                 foreach (['debug' => 10, 'info' => 50, 'warning' => 500] as $lv => $value) {
                     if ($event->length < $value) {
@@ -50,21 +63,20 @@ class AsyncRedisQueueListener implements ListenerInterface
                         break;
                     }
                 }
-
                 if ($event->length >= $value) {
                     Log::error($message);
                 }
                 break;
-            case 'Hyperf\\AsyncQueue\\Event\\FailedHandle':
+            case $event instanceof FailedHandle:
                 [$msg, $trace] = ['消息最终消费失败,原因为:' . $event->getThrowable()->getMessage(), $event->getThrowable()->getTrace()];
                 Log::error($msg, $trace);
                 break;
-            case 'Hyperf\\AsyncQueue\\Event\\RetryHandle':
+            case $event instanceof RetryHandle:
                 [$msg, $trace] = ['消息正在重试,原因为:' . $event->getThrowable()->getMessage(), $event->getThrowable()->getTrace()];
-                Log::error($msg, $trace);
+                Log::warning($msg, $trace);
                 break;
             default:
-                var_dump($event);
+                Log::warning('未知事件!!!');
         }
     }
 }
